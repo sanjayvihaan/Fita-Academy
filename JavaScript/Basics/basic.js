8843d79b524592d8fa94e694e9f52cb3c9db1997
V Sanjay <vsanjay@Vs-MacBook-Air.local>
2024-03-15 20:31:48 +0530

    Basics of Javascript 1

---
+++ JavaScript/Basics/basic.js
@@ -0,0 +1,71 @@
+console.log("hello world from external")
+
+var name = "sanjay"
+// var num = 1;
+// var num2 = 5;
+// let sum = num + num2;
+
+console.log(name);
+// console.log(1 + 5)
+
+// JavaScript syntax defines two types: - Single line comments
+    /* --- MultiLine Coments
+    1. Fixed values
+    2. Variable values
+
+    Fixed values: Literals
+    Variable values: Variables
+
+    Fixed values:
+    100, 1001 500 5.5
+
+    variable values: 
+    '2', "jdlkfjkaldf"
+    
+    */
+
+
+// let num = 4;
+// let num2 = 4
+
+// console.log("4 + 4");
+
+// -----------------------------------------------------
+
+//JavScript Variables: It is use to store data values
+
+// JavaScript variable keywords: var, let, const to declare variable: "="
+// Let - It store the values in the block scope level.
+// var - It store values in the functional scope leve.
+// const - we can't change the value once it is defined.
+
+let x = 6;
+console.log(x);
+
+x = 11;
+console.log(x);
+
+const num = 10;
+
+console.log(num);
+
+var name = "fita academy";
+console.log(name);
+
+let firstname, lastname;
+firstname = "V";
+lastname = "Sanjay"
+
+// console.log(firstname , lastname)
+
+// firstName , first_name, dateOfBrirth
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//Tomorrow's topic:
+    /* 
+    1. Variables - let, var, const
+    2. Operators
+    3. Data Types
+    4. Function
+    */
---
+++ JavaScript/readme.txt
@@ -0,0 +1,11 @@
+What is JavaScript?
+- JavaScript is the world's most popular programming language.
+
+- Why we use?
+    We have a website and also there is button or some logic behind the functioning
+
+
+    The Original JavaScript ES1 ES2 ES3 (1997-1999)
+    The First Main Revision ES5 (2009)
+    The Second Revision ES6 (2015)
+    The Yearly Additions (2016, 2017 ... 2021, 2022)
