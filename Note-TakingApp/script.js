cd24afa8f38bdf4d826109cf5eacfb535d1786c2
V Sanjay <vsanjay@Vs-MacBook-Air.local>
2024-04-02 13:23:29 +0530

    Completed the note taking project

--- Note-TakingApp/script.js
+++ Note-TakingApp/script.js
@@ -13,10 +13,7 @@ const addBtn = popupBox.querySelector("button");
 const months = ["January", "February", "March", "April", "May", "June", "July",
 "August", "September", "October", "November", "December"];
 
-//Local Storage:
-const notes = JSON.parse(localStorage.getItem("notes")|| "[]");
-let isUpdate = false;
-let updateId;
+
 
 
 
@@ -37,8 +34,69 @@ closeIcon.addEventListener("click", () => {
     popupBox.classList.remove('show');
     document.querySelector("body").style.overflow = "auto";
 })
+//Local Storage:
+const notes = JSON.parse(localStorage.getItem("notes")|| "[]");
+console.log(notes);
+let isUpdate = false;
+let updateId;
 
-// Getting the card
+//Function to show all the notes from local storage:
+function showNotes() {
+    if(!notes) return;
+    document.querySelectorAll(".note").forEach(li => li.remove());
+    notes.forEach((note, id) => {
+        let filterDesc = note.description.replaceAll("\n", '<br/>');
+        let liTag = `<li class="note">
+                        <div class="details">
+                            <p>${note.title}</p>
+                            <span>${filterDesc}</span>
+                        </div>
+                        <div class="bottom-content">
+                            <span>${note.date}</span>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="menu">
+                                    <li onclick="updateNote(${id}, '${note.title}', '${filterDesc}')"><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick="deleteNote(${id})"><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>`;
+        addBox.insertAdjacentHTML("afterend", liTag);
+    });
+}
+showNotes();
+
+function showMenu(elem) {
+    elem.parentElement.classList.add("show");
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != elem) {
+            elem.parentElement.classList.remove("show");
+        }
+    });
+}
+
+function deleteNote(noteId) {
+    let confirmDel = confirm("Are you sure you want to delete this note?");
+    if(!confirmDel) return;
+    notes.splice(noteId, 1);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    showNotes();
+}
+
+function updateNote(noteId, title, filterDesc) {
+    let description = filterDesc.replaceAll('<br/>', '\r\n');
+    updateId = noteId;
+    isUpdate = true;
+    addBox.click();
+    titleTag.value = title;
+    descTag.value = description;
+    popupTitle.innerText = "Update a Note";
+    addBtn.innerText = "Update Note";
+}
+
+
+// Getting the card data to the local storage
 
 addBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -46,23 +104,26 @@ addBtn.addEventListener("click", (e) => {
     let description = descTag.value.trim();
     console.log(title, description);
 
-    if(title || description) {
-        let currentDate = new Date(),
+    if (title || description) {
+      
+    
+
+    let currentDate = new Date(),
         month = months[currentDate.getMonth()],
         day = currentDate.getDate(),
-        year = currentDate.getFullYear(),
-        timeString = currentDate.toLocaleTimeString();
-        
+        year = currentDate.getFullYear()
+        // timeString = currentDate.toLocaleTimeString();
 
-        let noteInfo = {title, description, date: `${month} ${day} ${year} ${timeString}`}
-        if(!isUpdate) {
-            notes.push(noteInfo);
-        }else {
-            isUpdate = false;
-            notes[updateId] = noteInfo;
-        }
-
-        localStorage.setItem("notes", JSON.stringify(notes));
-        closeIcon.click();
+    let noteInfo = {title, description, date: `${month} ${day} ${year}`};
+    if (!isUpdate) {
+        notes.push(noteInfo);
+    } else {
+        isUpdate = false;
+        notes[updateId] = noteInfo;
     }
-})
+
+    localStorage.setItem("notes", JSON.stringify(notes));
+    closeIcon.click();
+
+}
+});
